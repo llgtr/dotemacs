3e6a2d53f3eba68a76b7e229aa35f114cb9da370
llgtr <llgtr@users.noreply.github.com>
2018-05-08 23:56:42 +0300

    Configure web-mode with non-offensive settings

--- user/init-lang.el
+++ user/init-lang.el
@@ -57,6 +57,14 @@
   :mode
   ("\\.html?\\'" . web-mode)
   ("\\.js?\\'" . web-mode)
-  ("\\.jsx?\\'" . web-mode))
+  ("\\.jsx?\\'" . web-mode)
+  :config
+  (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
 
 (provide 'init-lang)
